543146bb46cceec479ee68584e5ce7a430f8b310
Deni B <baccic@gmail.com>
2013-02-08 23:48:21 +0100

    Initial Commit

---
+++ Hours/HoursViewController.m
@@ -0,0 +1,29 @@
+//
+//  HoursViewController.m
+//  Hours
+//
+//  Created by Deni Bacic on 8. 02. 13.
+//  Copyright (c) 2013 Deni Bacic. All rights reserved.
+//
+
+#import "HoursViewController.h"
+
+@interface HoursViewController ()
+
+@end
+
+@implementation HoursViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Hours/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hours
+//
+//  Created by Deni Bacic on 8. 02. 13.
+//  Copyright (c) 2013 Deni Bacic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HoursAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HoursAppDelegate class]));
+    }
+}
